ae2186a068f1c4bf9b08656ce45fe495d3ab6779
adaugoglory <adaugogloryafor.com>
2023-04-23 15:50:27 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main- Entry point
-* Return: 0
-*/
+ * main- Entry point
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n")
