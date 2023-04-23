a6f196b109a31cee42a4de039f08ae6274a7546a
adaugoglory <adaugogloryafor.com>
2023-04-23 14:55:36 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
- * Return: Always 0
- */
-int main(void)
+* main - Entry point
+* Return: Always 0
+*/
+intmain(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programmingis like building a multilingual puzzle");
+return(0);
 
 }
